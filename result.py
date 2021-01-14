3310a0d7017b48cae6880dbc03c6e6fe3a7c0397
787277926 <787277926@qq.com>
2021-01-14 17:02:21 +0800

    触发jenkins测试13

--- result.py
+++ result.py
@@ -26,7 +26,7 @@ def send_mail(file_new):
     smtpserver = ''
     # 发送邮箱用户/
     mail_user = '151231@163.com'
-    mail_password = '123123'
+    mail_password = '1212312313123'
     # 发送邮箱
     sender = ''
     # 接收邮箱
